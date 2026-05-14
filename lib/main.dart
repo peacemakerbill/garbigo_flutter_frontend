39107c170187d1ce35a2e91d343692d1c7b015e9
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-05-15 01:54:14 +0300

    Removing # on urls

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:garbigo_frontend/core/constants/app_themes.dart';
 import 'package:garbigo_frontend/routing/app_router.dart';
 import 'package:garbigo_frontend/routing/router_notifier.dart';
@@ -13,6 +14,7 @@ final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy(); // removes # from URLs
 
   final prefs = await SharedPreferences.getInstance();
 
